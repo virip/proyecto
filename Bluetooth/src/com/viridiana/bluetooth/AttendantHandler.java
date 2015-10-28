5b375699df50bc3c2856191f3f357681d7b62cab
chesco134 <chesco134@gmail.com>
2015-10-28 01:15:40 -0600

    Arreglado problema de uso concurrente de la lista ventanaT1.

--- Bluetooth/src/com/viridiana/bluetooth/AttendantHandler.java
+++ Bluetooth/src/com/viridiana/bluetooth/AttendantHandler.java
@@ -11,7 +11,7 @@ import android.util.Log;
 import com.viridiana.thread.Filtrado;
 
 public class AttendantHandler extends Thread {
-	public static volatile Queue<Integer> ventanaT1 = new  LinkedList<Integer>();
+	public static Queue<Integer> ventanaT1 = new LinkedList<Integer>();
 	public Handler attendantThreadHandler;
 	Signals muestras;
 	Procesamiento proc = new Procesamiento();
@@ -22,13 +22,29 @@ public class AttendantHandler extends Thread {
 			@Override
 			public void handleMessage(Message msg) {
 				muestras = (Signals) msg.obj;
-				ventanaT1.add(muestras.getT1());
-				//Cálculo de la frecuencia
+				//Cï¿½lculo de la frecuencia
 				ServiceHandler.toracicaECG = proc.calcularFrecuencia(muestras.getT1(), ServiceHandler.toracicaECG);
 				//Dependiendo del orden del filtro
 				Log.d("window size", "" + ventanaT1.size());
-				if(ventanaT1.size()==8){
-				    new Filtrado(muestras).start();
+
+				synchronized(ventanaT1){
+					ventanaT1.add(muestras.getT1());
+					if(ventanaT1.size()==8){
+						Queue<Integer> nVentanT1 = new LinkedList<Integer>();
+							for(Integer sample : ventanaT1){
+								nVentanT1.add(sample);
+							}
+						ventanaT1.clear();
+						/*****************************************************************************************
+						 * 
+						 * 	Debes revisar que estÃ¡s pasando el paquete de 8 muestras junto con el paquete
+						 * actual de 5 muestras (actuales), que incluye a la 8va muestra (con la que acompletas
+						 * lo requerido para empezar a filtrar).
+						 * 
+						 *  
+						 ******************************************************************************************/
+						new Filtrado(muestras,nVentanT1).start();
+					}
 				}
 			}
 		};
